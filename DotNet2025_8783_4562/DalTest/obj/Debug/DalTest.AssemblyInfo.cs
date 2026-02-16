1e20737014ed5243cc3eb4d40ed4ddc39af5f644
ystroh <y0534178779@example.com>
2026-02-16 20:10:56 +0200

    m

--- DotNet2025_8783_4562/DalTest/obj/Debug/DalTest.AssemblyInfo.cs
+++ DotNet2025_8783_4562/DalTest/obj/Debug/DalTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DalTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+600a897baff7971c4ab93290f1b4454a5f5777ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60be6d5ed9731aaa5d9357050ff929857e5bfc10")]
 [assembly: System.Reflection.AssemblyProductAttribute("DalTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DalTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
