a3d4e5ce2bb6eb7f3650729268e1c1627ed7d939
הנדסת תוכנה <huser@sem.local>
2026-03-11 17:19:43 +0200

    save

---
+++ DotNet2025_8783_4562/Tools/LogManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+
+
+namespace Tools;
+
+public static class LogManager
+  {
+    private static string path = "Log";
+    public static  string todayPathFile() {
+        return DateTime.Now.Day.ToString();
+    }
+    public static string monthPathDirectory() {
+        return DateTime.Now.Month.ToString();
+    }
+    public static void writeToLog(string projectName, string funcName, string message) {
+        string monthDir=monthPathDirectory();
+        string dayPath=todayPathFile();
+        string dirMonthPath= Path.Combine(path.FullName, monthDir);
+        if (!Directory.Exists(dirMonthPath))
+        {
+            Directory.createDirectory(dirMonthPath);
+        }
+        string fileDayPath = Path.Combine(dirMonthPath, $"{dayPath}.log");
+        using (StreamWriter writer = new StreamWriter(fileDayPath, true))
+        {
+
+            writer.WriteLine($"{DateTime.Now}\t{projectName}.{funcName}:\t{message}");
+        }
+        }
+    
+    }
